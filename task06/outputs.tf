6f474adf557c475d58507beb3fc43a8128c9dc31
TigranAHarutyunyan <tigran.harutunyan.02@gmail.com>
2026-06-19 21:56:20 +0400

    task 6 is done 3

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -6,3 +6,10 @@ output "random_password" {
   value       = random_password.sql_admin_password.result
   description = "random password "
 }
+output "sql_server_fqdn" {
+  value = module.sql.sql_server_fqdn
+}
+
+output "app_hostname" {
+  value = module.webapp.app_hostname
+}
