21f5f625be65e62571770a69e71b429e78645b4c
TigranAHarutyunyan <tigran.harutunyan.02@gmail.com>
2026-06-19 21:58:58 +0400

    task 6 is done ձ

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -2,10 +2,6 @@ output "tenant_id" {
   value       = data.azurerm_client_config.current.tenant_id
   description = "Tenat Id "
 }
-output "random_password" {
-  value       = random_password.sql_admin_password.result
-  description = "random password "
-}
 output "sql_server_fqdn" {
   value = module.sql.sql_server_fqdn
 }
